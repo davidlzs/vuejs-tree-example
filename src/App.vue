bc1051a10be938d7a0245b561c278d76462306b5
David L <davidlzs@hotmail.com>
2017-02-02 15:23:59 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <ul>
+      <item class="item" :model="treeData"/>
+    <ul>
+  </div>
+</template>
+
+<script>
+import Item from './components/Item'
+
+var data = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+}
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      treeData: data
+    }
+  },
+  components: {
+    Item
+  }
+}
+</script>
+
+<style>
+.body {
+  font-family: Menlo, Consolas, monospace;
+  color: #444;
+}
+.item {
+  cursor: pointer;
+}
+.bold {
+  font-weight: bold;
+}
+ul {
+  padding-left: 1em;
+  line-height: 1.5em;
+  list-style-type: dot;
+}
+</style>
